0dfc384364a36299e37bdf1b83d16277c14d428e
William Uyeta <william.uyeta@securid.com>
2023-12-29 10:53:44 -0800

    fixed the black background on iphone

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -7,6 +7,8 @@ import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Experience from './components/Experience';
 import Footer from './components/Footer';
+import './App.css'
+
 const App = () => {
   return (
     <div className="font-sans text-gray-900">
